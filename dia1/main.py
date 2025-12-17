62973faf231b75fe77bc2b591a45bbcbb0485306
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2025-12-16 21:43:47 -0300

    finalizei o dia 1 do desafio

--- dia1/main.py
+++ dia1/main.py
@@ -1,1 +1,11 @@
-print("Olá Mundo!")
+# Calculadora seja simples contendo somente as principais operações aritiméteicas
+
+# ENTRADA 
+numero1 = int(input("Digite um número: ")) # 5
+numero2 = int(input("Digite outro número: "))#6
+
+# PROCESSAMENTO
+soma = numero1 + numero2
+
+# SAÍDA
+print(f"O Resultado da soma é igual a {soma}")
